978503663ef7af5853c19abac812319f2712f762
Tamsen1995 <Tamanhbui95@gmail.com>
2017-12-19 19:06:35 +0100

    [UPD] Made a linked list to keep track of the cursor position, it will always dynamically be the size of the window column size

--- src/prompt/prompt_loop.c
+++ src/prompt/prompt_loop.c
@@ -28,6 +28,94 @@ struct winsize		*get_win_size()
     return (ret);
 }
 
+////////////////// CURSOR ACTIONS
+
+
+
+/*
+** allocates a new cursor list element
+*/
+
+t_cursor      *ft_new_cur(void)
+{
+    t_cursor *new;
+
+    if (!(new = (t_cursor *)malloc(sizeof(t_cursor))))
+        fatal("Error: could not allocate env var list elem in ft_new_env");
+    new->next = NULL;
+    new->prev = NULL;
+    new->cursor = FALSE;
+    return (new);
+}
+
+/*
+** pushes a cursor element to the end of a list
+*/
+
+void		ft_add_cursor(t_cursor **begin_list)
+{
+    t_cursor *tmp;
+
+    tmp = NULL;
+	if (!*begin_list)
+	{
+		*begin_list = ft_new_cur();
+		return ;
+	}
+    else
+    {
+        tmp = *begin_list;
+        while (tmp->next)
+            tmp = tmp->next;
+        tmp->next = ft_new_cur();
+        tmp->next->prev = tmp;
+    }
+}
+
+/*
+** returns a linked list which will be used to keep track
+** of the cursor
+** each list element represents a potential cursor position
+** the length of the list will be that of the columns 
+** within the temrinal window
+*/
+
+t_cursor       *init_cursor(int win_size) // WIP
+{
+    int i;
+    t_cursor *cur_list;
+
+    i = 0;
+    cur_list = NULL;
+    if (!win_size)
+        fatal("Error (init_cursor)");
+    while (i < win_size)
+    {
+        ft_add_cursor(&cur_list);
+        i++;
+    }
+    return (cur_list);
+}
+
+////////////////// CURSOR ACTIONS
+
+// TESTING purposes
+void           test_cursor(t_line *line)
+{
+    int i;
+    t_cursor *tmp;
+
+    i = 0;
+    tmp = line->cursor;
+    printf("\n\n - > %d\n\n", line->sz->ws_col);
+    while (tmp)
+    {
+        i++;
+        tmp = tmp->next;
+    }
+    printf("\n\n - > %d\n\n", i);
+}
+
 t_line         *init_line()
 {
     t_line *line;
@@ -40,12 +128,7 @@ t_line         *init_line()
     line->prompt = "tamsshell $> ";
     line->prompt_len = ft_strlen(line->prompt);
     line->sz = get_win_size();
-
-    // printf("%d\n", line->sz->ws_col);
-    
-    // initiate the cursor with the help of
-    // prompt len and the column size
-
+    line->cursor = init_cursor((int)line->sz->ws_col);
     return (line);
 }
 
@@ -74,9 +157,10 @@ char            *prompt_loop(void) // WIP
         // edit the buffer according to action requested
         check_input(line, buf); // WIP
         if (term_action(buf) == FALSE) // WIP
+        {
             ft_add_buf(&line->buffer, buf);
-        if (term_action(buf) == FALSE) // WIP
             print_buffer(line->buffer);
+        }
     }
     cmd_line = stringify_buffer(line->buffer);
 
