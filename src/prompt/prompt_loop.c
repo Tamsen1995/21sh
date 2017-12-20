cfa9775685ce98d6b5f9754b692107255e80f1e2
Tamsen1995 <Tamanhbui95@gmail.com>
2017-12-20 15:47:09 +0100

    [ADD] Adding a small module which will free the structure needed for line edition

--- src/prompt/prompt_loop.c
+++ src/prompt/prompt_loop.c
@@ -48,18 +48,51 @@ t_line         *init_line()
     return (line);
 }
 
+
+
+///////////////////////////// FREE LINE STRUCT
+
+/*
+** freeing the line buffer
+*/
+
+void            free_line_buf(t_line *line)
+{
+    t_buf *tmp;
+
+    tmp = line->buffer;
+    while (tmp->next)
+        tmp = tmp->next;
+    while (tmp->prev)
+    {
+        tmp = tmp->prev;
+        ft_strfree(tmp->next->key);
+        free(tmp->next);
+    }
+    ft_strfree(tmp->key);
+    free(tmp);
+}
+
 /*
 ** function which frees the line edition
 ** structure
 */
 
+
 void            free_line_struct(t_line *line)
 {
     if (!line)
         fatal("Error (free_line_struct)");
+    free_line_buf(line); // WIP
+  //  free_cursor(line); // WIP
+    //free_win_sz();
+    
     // TODO finish function
 }
 
+
+
+///////////////////////////// FREE LINE STRUCT
 
 /*
 ** Display the initial contents of the text buffer on the screen.
@@ -92,6 +125,6 @@ char            *prompt_loop(void)
         }
     }
     cmd_line = stringify_buffer(line->buffer);
-    // free_line_struct(line);
+    free_line_struct(line);
     return (cmd_line);
 }
