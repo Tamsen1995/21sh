12647ed8a3f0e2c158881c6e29aa7234f0f97239
Tamsen1995 <Tamanhbui95@gmail.com>
2017-12-24 01:14:11 +0100

    [UPD]

--- src/prompt/line_edition/line_buffer/del_buf_elem.c
+++ src/prompt/line_edition/line_buffer/del_buf_elem.c
@@ -6,18 +6,18 @@
 ** the current tracked cursor position
 */
 
-t_buf       *get_cur_buffer(t_line *line)
+t_buf       *get_cur_buffer(t_line **line)
 {
     t_buf       *ret;
     t_cursor    *tmp_cur;
 
     ret = NULL;
     tmp_cur = NULL;
-    if (!line || !line->first_c || !line->current_c)
+    if (!(*line) || !(*line)->first_c || !(*line)->current_c)
         fatal("Error (get_cur_buffer)");
-    ret = line->buffer;
-    tmp_cur = line->first_c;
-    while (ret->next && tmp_cur && tmp_cur->c_ind != line->current_c->c_ind)
+    ret = (*line)->buffer;
+    tmp_cur = (*line)->first_c;
+    while (ret->next && tmp_cur && tmp_cur->c_ind != (*line)->current_c->c_ind)
     {
         ret = ret->next;
         tmp_cur = tmp_cur->next;
@@ -43,31 +43,30 @@ void            free_buf_elem(t_buf *buf_elem)
 void           del_buf_elem(t_line **line)
 {
     t_buf *del;
-    t_buf *tmp;
 
     del = NULL;
-    tmp = NULL;
     if (!(*line) || !(*line)->buffer || !(*line)->cursor)
         fatal("Error (del_buf_elem)");
-    del = get_cur_buffer((*line));
-    tmp = (*line)->buffer;
+    del = get_cur_buffer(line);
     if (del->ind == (*line)->buffer->ind || !del->prev)
         return ;
     if (del->next)
         del = del->prev;
-    while (tmp->ind != del->ind)
-        tmp = tmp->next;
     if (!del->next)
+        del->prev->next = NULL;
+    else
     {
-     //   free_buf_elem(tmp);
-     // somehow free the list
-        tmp->prev->next = NULL;
-        (*line)->current_c = (*line)->current_c->prev;
-        return ;
+        del->prev->next = del->next;
+        del->next->prev = del->prev;
     }
-// TODO fix this function (make it work!)
+    free_buf_elem(del);
+    (*line)->current_c = (*line)->current_c->prev;
 
+
+    // TODO re-iterate through the list and reset buffer indexes
+
+    // TODO fix this function (make it work!)
 }
 
 // TODO insert function
-//void        insert_at_cursor();
+// void        insert_at_cursor();
