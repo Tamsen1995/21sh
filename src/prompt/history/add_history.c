07543726493c1cbb0e7b0f0dc63aa1b23e0d9424
Tamsen1995 <Tamanhbui95@gmail.com>
2017-12-29 22:59:18 +0100

    [UPD] I can iterate through my history now as well have the cursor me updated dynamically. Also the history list will start from the beginning now once a command has been executed. (check_history)

--- src/prompt/history/add_history.c
+++ src/prompt/history/add_history.c
@@ -21,6 +21,26 @@ t_hist *ft_new_his(char *cmd)
     return (new);
 }
 
+/*
+** sets all the ->current pointers in the
+** history linked list to false
+*/
+
+void        reset_hist_positions(t_hist *history)
+{
+    t_hist *tmp;
+
+    tmp = NULL;
+    if (!history)
+        fatal("Error (reset_hist_positions)");
+    tmp = history;
+    while (tmp)
+    {
+        tmp->current = FALSE;
+        tmp = tmp->next;
+    }
+}
+
 
 /*
 ** adds an element to the end of the history list
@@ -46,7 +66,7 @@ void        add_history(t_hist **history, char *cmd)
     new_his = ft_new_his(cmd);
     while (tmp_his->next)
         tmp_his = tmp_his->next;
-    tmp_his->current = FALSE;
+    reset_hist_positions((*history));
     tmp_his->next = new_his;
     new_his->prev = tmp_his;
 }
