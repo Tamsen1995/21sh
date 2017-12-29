629c0ff753af93a503b5036ca6072d79933e9886
Tamsen1995 <Tamanhbui95@gmail.com>
2017-12-29 16:45:26 +0100

    [UPD] The add_history function now works and adds each executed command onto the history linked list accordingly. next up is the check_history function (add_history) (check_history)

--- src/prompt/history/add_history.c
+++ src/prompt/history/add_history.c
@@ -1,14 +1,49 @@
 #include "../../../includes/ft_sh.h"
 
+
+/*
+** allocates a new buffer list element
+*/
+
+t_hist *ft_new_his(char *cmd)
+{
+    t_hist *new;
+
+    if (!(new = (t_hist *)malloc(sizeof(t_hist))))
+        fatal("Error: could not allocate env var list elem in ft_new_env");
+    new->next = NULL;
+    new->prev = NULL;
+    new->cmd = NULL;
+    new->current = FALSE;
+    new->cmd = ft_strdup(cmd);
+    return (new);
+}
+
+
 /*
 ** adds an element to the end of the history list
 ** and makes every element of it false, except for the
 ** last one which is to be set to true.
 */
 
-void        add_history(t_hist **history, char *buf)
+void        add_history(t_hist **history, char *cmd)
 {
-    if (!history || !buf)
+    t_hist *new_his;
+    t_hist *tmp_his;
+
+    new_his = NULL;
+    tmp_his = NULL;
+    if (!history || !cmd)
         return ;
-    // TODO : finish
+    if (!(*history))
+    {
+        (*history) = ft_new_his(cmd);
+        return ;
+    }
+    tmp_his = (*history);
+    new_his = ft_new_his(cmd);
+    while (tmp_his->next)
+        tmp_his = tmp_his->next;
+    tmp_his->next = new_his;
+    new_his->prev = tmp_his;
 }
