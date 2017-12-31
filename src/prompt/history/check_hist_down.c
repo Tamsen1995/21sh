d763b95ee6cfeed381c6b91443732da6adc8c612
Tamsen1995 <Tamanhbui95@gmail.com>
2017-12-31 01:47:51 +0100

    [UPD] Buffer needs to be deleted in (check_hist_down)

--- src/prompt/history/check_hist_down.c
+++ src/prompt/history/check_hist_down.c
@@ -9,10 +9,18 @@ void    check_hist_down(t_line *line)
         fatal("Error (check_hist_down)");
     if (!line->history)
         return ;
+    if (line->his_index == 0)
+        return ;
+    else if (get_last_index(line->history) == line->his_index)
+    {
+        fatal("Buffer needs to be deleted in (check_hist_down)");
+      // TODO : delete the buffer
+    }
+    else
+        line->his_index++;
     tmp_his = line->history;
-
-
-
+    while (tmp_his && tmp_his->index != line->his_index)
+        tmp_his = tmp_his->next;
     free_buffer(line->buffer);
     line->buffer = replace_buffer(tmp_his->cmd);
     set_cursor_internal(line);
