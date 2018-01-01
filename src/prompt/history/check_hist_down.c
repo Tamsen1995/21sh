4c898bab7940d6d1944a6f98a460c02cdf6da857
Tamsen1995 <Tamanhbui95@gmail.com>
2018-01-01 18:53:12 +0100

    [UPD] the history module seems to be working perfectly fine now.

--- src/prompt/history/check_hist_down.c
+++ src/prompt/history/check_hist_down.c
@@ -2,9 +2,11 @@
 
 void    check_hist_down(t_line *line)
 {
-    t_hist *tmp_his;
+    t_hist  *tmp_his;
+    char    *replacement;
 
     tmp_his = NULL;
+    replacement = NULL;
     if (!line)
         fatal("Error (check_hist_down)");
     if (!line->history)
@@ -12,16 +14,16 @@ void    check_hist_down(t_line *line)
     if (line->his_index == 0)
         return ;
     else if (get_last_index(line->history) == line->his_index)
-    {
-        fatal("Buffer needs to be deleted in (check_hist_down)");
-      // TODO : delete the buffer
-    }
+        replacement = "";
     else
+    {
         line->his_index++;
-    tmp_his = line->history;
-    while (tmp_his && tmp_his->index != line->his_index)
-        tmp_his = tmp_his->next;
+        tmp_his = line->history;
+        while (tmp_his && tmp_his->index != line->his_index)
+            tmp_his = tmp_his->next;
+        replacement = tmp_his->cmd;
+    }
     free_buffer(line->buffer);
-    line->buffer = replace_buffer(tmp_his->cmd);
+    line->buffer = replace_buffer(replacement);
     set_cursor_internal(line);
 }
