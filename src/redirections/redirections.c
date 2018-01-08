5f7525ab6ff5c73d945f946c1f47628ba7b50715
Tamsen1995 <Tamanhbui95@gmail.com>
2018-01-08 19:22:13 +0100

    [UPD] (redirections.c) We are starting to implement the handling of the redirection of inputs. A linked list is to be made which will store all the fds needed for appropiate redirection.

--- src/redirections/redirections.c
+++ src/redirections/redirections.c
@@ -10,6 +10,28 @@ T_BOOL			is_input_redir(char *redir, char *file)
 	return (FALSE);
 }
 
+
+void			reset_fd_arr(int *fd_arr)
+{
+	fd_arr[0] = 0;
+	fd_arr[1] = 1;
+	fd_arr[2] = 2;
+}
+
+
+int				get_fd(char *file)
+{
+	int		fd;
+	mode_t	mode;
+
+	fd = 0;
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	if ((fd = open(file, O_CREAT | O_RDWR | O_TRUNC, mode)) < 0)
+		fatal("Could not open in (get_fd)");
+	return (fd);
+}
+
+
 /*
 ** will discern the redirections 
 */
@@ -18,15 +40,19 @@ void 			discern_redirs(t_shell *shell)
 {
 	int		i;
     char	**cmd_args;
+	int		fd_arr[3];
 
 	i = 0;
     cmd_args = shell->cmds->args;
 	while (cmd_args[i + 1] && cmd_args[i])
 	{
+		reset_fd_arr(fd_arr);
 		if (is_input_redir(cmd_args[i], cmd_args[i + 1]))
-
+			fd_arr[0] = get_fd(cmd_args[i + 1]);
 		i++;
 	}
+	
+
 	ft_putendl("EXITING PROGRAM IN (discern_redirs)");
 	exit(-1);
 }
