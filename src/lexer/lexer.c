1870085d54b2ecbf1577624f93e4c3b2e2ac4119
Tamanh BUI <tbui@e1r10p3.42.fr>
2018-01-09 16:40:38 +0100

    [ADD] Adding lexer

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,14 +1,6 @@
 #include "../../includes/ft_sh.h"
 
-enum e_redir	redir_type(char *redir, char *file)
+enum e_tokentype	tok_type()
 {
-	if (!redir || !file)
-		fatal("Error (redir_type)");
-	if (ft_strcmp(redir , "<") == 0 && access(file, W_OK) == 0)
-		return (INPUT_RE);
-	if (ft_strcmp(redir , ">") == 0 && access(file, W_OK) == 0)
-		return (OUTPUT_RE);
-	if (ft_strcmp(redir , "2>") == 0 && access(file, W_OK) == 0)
-		return (ERR_RE);
-	return (NO_REDIRECTION);
+
 }
