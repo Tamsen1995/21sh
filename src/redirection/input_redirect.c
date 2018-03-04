f08630f04e59f9764444ac130deaa3b9a304d652
Tamanh BUI <tbui@e1r4p19.42.fr>
2018-03-04 04:54:15 +0100

    [UPD] the input_redirect flow is starting to come to life. I need to now fork the process and implement the pipe from the parent process into the child process so that the child may execute the input that is given through the pipe from the parent process. I'm changing the way the environment array is passed into the execution flow so that it may be passed along with the shell structure. This gives for cleaner and more concise code.

--- src/redirection/input_redirect.c
+++ src/redirection/input_redirect.c
@@ -4,7 +4,6 @@ void dup_exec(t_shell *shell, int *pipe_fd, int fd_left)
 {
 	int save_fd;
 
-
 	save_fd = dup(fd_left);
 	close(pipe_fd[fd_left == STDOUT_FILENO ? 0 : 1]); // closing write end because this process will only read from the pipe
 	dup2(pipe_fd[fd_left == STDOUT_FILENO ? 1 : 0], fd_left);
@@ -14,19 +13,9 @@ void dup_exec(t_shell *shell, int *pipe_fd, int fd_left)
 	// execute command recursively
 
 	// make the fd_left into the save_fd (its initial value) again
-
-	// close the save fd
-
-	// make the process wait or exit
-
-	// 	exec_cmd(cmd);
-	// dup2(save_fd, fd_left);
-	// close(save_fd);
-	// fd_left == STDOUT_FILENO ? (void)wait(NULL) : exit(EXIT_SUCCESS);
-
-	// closes the fd left
-
-	//
+	dup2(save_fd, fd_left);
+	close(save_fd);
+	fd_left == STDOUT_FILENO ? (void)wait(NULL) : exit(EXIT_SUCCESS);
 }
 
 /*
@@ -47,18 +36,12 @@ static void fork_that(t_shell *shell, int fd_left, int fd_right)
 		fatal("Could not open pipe in (fork_that)");
 	pid = fork();
 	if (pid == 0)
-	{
 		dup_exec(shell, pipe_fd, fd_left);
-		// replaces the fd_left with the either read or write end of the pipe depending
-		// on what needs replacing, then executes the command with the modified
-		// fds
-		// the write to pipe function below writes to the other end of the pipe.
-	}
 	else if (pid < 0)
 		fatal("Child procress could not be created (sh_launch)");
 	else
 	{
-		// write to the pipe with the fd_right
+		write_to_pipe(shell, pipe_fd, fd_left);
 		wpid = waitpid(pid, &status, WUNTRACED);
 		while (!WIFEXITED(status) && !WIFSIGNALED(status))
 			wpid = waitpid(pid, &status, WUNTRACED);
