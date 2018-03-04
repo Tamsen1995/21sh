4df9cddbd62ef0363a7d99d5b2234402aaa76b11
Tamanh BUI <tbui@e1r4p19.42.fr>
2018-03-04 01:39:38 +0100

    [UPD] the input_redirect flow is starting to come to life. I need to now fork the process and implement the pipe from the parent process into the child process so that the child may execute the input that is given through the pipe from the parent process.

--- src/redirection/input_redirect.c
+++ src/redirection/input_redirect.c
@@ -1,7 +1,78 @@
 #include "../../includes/ft_sh.h"
 
+void dup_exec(char **cmd, int *pipe_fd, int fd_left)
+{
+	int save_fd;
+
+	save_fd = dup(fd_left);
+	close(pipe_fd[fd_left == STDOUT_FILENO ? 0 : 1]); // closing write end because this process will only read from the pipe
+	dup2(pipe_fd[fd_left == STDOUT_FILENO ? 1 : 0], fd_left);
+	close(pipe_fd[fd_left == STDOUT_FILENO ? 1 : 0]);
+
+
+	// execute command recursively
+
+	// make the fd_left into the save_fd (its initial value) again
+	
+	// close the save fd
+
+	// make the process wait or exit
+
+	// 	exec_cmd(cmd);
+	// dup2(save_fd, fd_left);
+	// close(save_fd);
+	// fd_left == STDOUT_FILENO ? (void)wait(NULL) : exit(EXIT_SUCCESS);
+
+	// closes the fd left
+
+	//
+}
+
+/*
+** forks the process so that the parent may write from the fd_right into the pipe
+** the child process will READ from the pipe and then 
+** execute the command with this pipe set up
+*/
+
 static void fork_that(char **cmd, int fd_left, int fd_right)
 {
+	pid_t pid;
+	pid_t wpid;
+	int status;
+	int pipe_fd[2];
+
+	if (pipe(pipe_fd) < 0)
+		fatal("Could not open pipe in (fork_that)");
+	pid = fork();
+	if (pid == 0)
+	{
+		// replace the fd_left with the read end of the pipe
+		// set the child process up to read from the reading end of the pipe
+	}
+	else if (pid < 0)
+		fatal("Child procress could not be created (sh_launch)");
+	else
+	{
+		// write to the pipe with the fd_right
+		wpid = waitpid(pid, &status, WUNTRACED);
+		while (!WIFEXITED(status) && !WIFSIGNALED(status))
+			wpid = waitpid(pid, &status, WUNTRACED);
+	}
+}
+
+// TEST function
+int get_fd(char *s)
+{
+	int fd;
+
+	if (!s)
+		return (-1);
+	while (*s && !ft_isdigit(*s) && *s != '-')
+		s++;
+	if (!*s)
+		return (-1);
+	fd = ft_atoi(s);
+	return (fcntl(fd, F_GETFD) ? -1 : fd);
 }
 
 /*
@@ -10,6 +81,7 @@ static void fork_that(char **cmd, int fd_left, int fd_right)
 ** stores these in two seperate variables (fd_left and fd_right)
 ** deletes the redirection symbol as well as the redirection file from the cmds array
 */
+
 void do_redirect(char **cmd, enum e_replacement c, int o_flag, void (*do_stuff_with_cmd_and_fd_now)(char **, int, int))
 {
 	int i;
@@ -21,7 +93,7 @@ void do_redirect(char **cmd, enum e_replacement c, int o_flag, void (*do_stuff_w
 	fd_left = 42; // TESTING
 	while (cmd[i] && !(tmp = ft_strchr(cmd[i], c)))
 		i++;
-	if (ft_strcmp(cmd[i], tmp) == 0)
+	if ((ft_strcmp(cmd[i], tmp) == 0) || (fd_left = get_fd(cmd[i])) == -1)
 		fd_left = c == R_INPUT ? STDIN_FILENO : STDOUT_FILENO;
 	if ((fd_right = open(cmd[i + 1], o_flag, 0664)) == -1) // getting the fd of the file that is supposed to replace the initial fd
 		fatal("Could not get fd in (do_redirect)");
@@ -30,6 +102,8 @@ void do_redirect(char **cmd, enum e_replacement c, int o_flag, void (*do_stuff_w
 		cmd = ft_arrdelone(cmd, cmd[i + 1]); // deletes the the file whose fd is now inside of fd_right
 		cmd = ft_arrdelone(cmd, tmp);		 // rid the cmds array of the redirection sign as well as
 	}
+
+	do_stuff_with_cmd_and_fd_now(cmd, fd_left, fd_right);
 }
 
 // calls the do_redirect(cmd, R_INPUT, O_RDONLY, fork_that); function
